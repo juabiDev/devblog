b885d25c7f6eca5c0c4a93bfe94240b3f11d914b
juabidev <Mederos40@outlook.com>
2025-02-23 11:46:33 -0300

    feat: add Comment class

--- Entities/Comment.cs
+++ Entities/Comment.cs
@@ -7,5 +7,9 @@
         public DateTime CreatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public User Author { get; set; }
+        public Guid AuthorId { get; set; }
+        public Post Post { get; set; }
+        public Guid PostId { get; set; }
     }
 }
