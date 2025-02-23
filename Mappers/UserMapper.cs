fc28111f840419947da607a286a135ff88eac6d6
juabidev <Mederos40@outlook.com>
2025-02-23 11:49:34 -0300

    feat: add class to map User to UserDTO and UserDTO to User

---
+++ Mappers/UserMapper.cs
@@ -0,0 +1,32 @@
+﻿using DevBlog.DTOs;
+using DevBlog.Entities;
+
+namespace DevBlog.Mappers
+{
+    public static class UserMapper
+    {
+        public static User MapToEntity(UserDTO user)
+        {
+            return new User
+            {
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                ProfilePhoto = user.ProfilePhoto,
+                About = user.About
+            };
+        }
+
+        public static UserDTO MapToDTO(User user)
+        {
+            return new UserDTO
+            {
+                Name = user.Name,
+                LastName = user.LastName,
+                Email = user.Email,
+                ProfilePhoto = user.ProfilePhoto,
+                About = user.About
+            };
+        }
+    }
+}
