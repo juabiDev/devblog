0fe01d6983fb653a2261866bfc058ea1bee91d5f
juabidev <Mederos40@outlook.com>
2025-02-23 11:52:01 -0300

    feat: add IUserService interface

---
+++ ServicesContract/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using DevBlog.DTOs;
+
+namespace DevBlog.ServicesContract
+{
+    public interface IUserService
+    {
+        public List<UserDTO> GetAllUsers();
+        public UserDTO GetUserById(Guid id);
+        public void DeleteUser(Guid id);
+        public void EditUser(UserDTO user);
+        public void AddUser(UserDTO id);
+    }
+}
