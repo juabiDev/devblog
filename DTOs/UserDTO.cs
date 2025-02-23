6d68473ff158fa50bd7931a3911fbc8c190cca17
juabidev <Mederos40@outlook.com>
2025-02-23 11:45:56 -0300

    feat: add UserDTO to hide sensitive information

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DevBlog.DTOs
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ProfilePhoto { get; set; }
+        public string About { get; set; }
+    }
+}
