6caa8c7bf67a9280d4bfc45e21a359b1db9e766d
juabidev <Mederos40@outlook.com>
2025-02-23 11:46:53 -0300

    feat: add Follow class to manage user Followers

---
+++ Entities/Follow.cs
@@ -0,0 +1,10 @@
+﻿namespace DevBlog.Entities
+{
+    public class Follow
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid FollowerId { get; set; }
+        public User Follower { get; set; }
+    }
+}
