6d449008c8a1aba36b2d8c528882e2433047acc3
juabidev <Mederos40@outlook.com>
2025-02-23 11:47:14 -0300

    feat: add Reaction class

---
+++ Entities/Reaction.cs
@@ -0,0 +1,11 @@
+﻿using DevBlog.Enum;
+
+namespace DevBlog.Entities
+{
+    public class Reaction
+    {
+        public Guid Id { get; set; }
+        public ReactionType ReactionType { get; set; }
+        public User? User { get; set; }
+    }
+}
