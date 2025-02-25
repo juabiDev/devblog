1100dd6a6b884798eab5be1cef0f66d6da60fd4e
juabidev <Mederos40@outlook.com>
2025-02-25 05:45:05 -0300

    refactor: change contract to Task metods

--- ServicesContract/IUserService.cs
+++ ServicesContract/IUserService.cs
@@ -1,13 +1,14 @@
 ﻿using DevBlog.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevBlog.ServicesContract
 {
     public interface IUserService
     {
-        public List<UserDTO> GetAllUsers();
-        public UserDTO GetUserById(Guid id);
-        public void DeleteUser(Guid id);
-        public void EditUser(UserDTO user);
+        Task<List<UserDTO>> GetAllUsersAsync();
+        Task<UserDTO?> GetUserByIdAsync(Guid id);
+        Task DeleteUserAsync(Guid id);
+        Task EditUserAsync(UserDTO user);
         Task AddUserAsync(UserDTO user);
     }
 }
