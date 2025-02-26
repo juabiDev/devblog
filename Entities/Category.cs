b30e86e7085b82c318fa5b55113a879697102a60
juabidev <Mederos40@outlook.com>
2025-02-26 08:25:54 -0300

    refactor: change property name

--- Entities/Category.cs
+++ Entities/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string name { get; set; }
         public ICollection<Post> Posts { get; set; } = new List<Post>();
     }
 }
