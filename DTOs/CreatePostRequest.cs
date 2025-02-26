ca98955de2fdded22c82b89cb105a34377415ef5
juabidev <Mederos40@outlook.com>
2025-02-26 08:22:40 -0300

    feat: add CreatePostRequest to hide private data and simplify post creation

---
+++ DTOs/CreatePostRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace DevBlog.DTOs
+{
+    public class CreatePostRequest
+    {
+        public Guid AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
+        public ICollection<Guid> Tags { get; set; } = new List<Guid>();
+    }
+}
