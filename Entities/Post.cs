89ba4ae7b3832e591a7e2eab7fb94aafc8005d25
juabidev <Mederos40@outlook.com>
2025-02-26 08:22:57 -0300

    refactor: change property name

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -9,11 +9,10 @@
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
-
         public Guid AuthorId { get; set; }
         public User Author { get; set; }
         public Guid CategoryId { get; set; }
-        public Category category { get; set; }
+        public Category Category { get; set; }
         public ICollection<Tag> Tags { get; set; } = new List<Tag>();
         public ICollection<Comment> Comments { get; set; } = new List<Comment>();
     }
---
+++ Mappers/TagMapper.cs
@@ -0,0 +1,25 @@
+﻿using DevBlog.DTOs;
+using DevBlog.Entities;
+
+namespace DevBlog.Mappers
+{
+    public static class TagMapper
+    {
+        public static TagDTO ToDTO(Tag tag)
+        {
+            return new TagDTO
+            {
+                Id = tag.Id,
+                Name = tag.Name
+            };
+        }
+        public static Tag ToEntity(TagDTO tagDTO)
+        {
+            return new Tag
+            {
+                Id = tagDTO.Id,
+                Name = tagDTO.Name
+            };
+        }
+    }
+}
