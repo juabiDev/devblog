0c4cd18b8d38bd6c0df6c737c4d49ef63e645ae9
juabidev <Mederos40@outlook.com>
2025-02-26 08:21:58 -0300

    feat: add PostDTO to hide private data

---
+++ DTOs/PostDTO.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DevBlog.DTOs
+{
+    public class PostDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string AuthorName { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<TagDTO> Tags { get; set; } = new List<TagDTO>();
+        public ICollection<CommentDTO> Comments { get; set; } = new List<CommentDTO>();
+    }
+}
