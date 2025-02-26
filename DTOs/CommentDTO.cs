290b3c21f5c3b9c40eb199d558802b23805b4a65
juabidev <Mederos40@outlook.com>
2025-02-26 08:21:47 -0300

    feat: add CommentDTO to hide private data

---
+++ DTOs/CommentDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DevBlog.DTOs
+{
+    public class CommentDTO
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorEmail { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorProfilePhoto { get; set; }
+    }
+}
