fdd90f8600d79b2f4e56661ad14f32941fb24d21
juabidev <Mederos40@outlook.com>
2025-02-28 06:50:01 -0300

    refactor: change methods names

--- Mappers/UserMapper.cs
+++ Mappers/UserMapper.cs
@@ -5,7 +5,7 @@ namespace DevBlog.Mappers
 {
     public static class UserMapper
     {
-        public static User MapToEntity(UserDTO user)
+        public static User ToEntity(UserDTO user)
         {
             return new User
             {
@@ -17,7 +17,7 @@ namespace DevBlog.Mappers
             };
         }
 
-        public static UserDTO MapToDTO(User user)
+        public static UserDTO ToDTO(User user)
         {
             return new UserDTO
             {
