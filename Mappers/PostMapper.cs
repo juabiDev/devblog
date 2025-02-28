34ff4800d6dc5d550826b306cab2a713e12227f7
juabidev <Mederos40@outlook.com>
2025-02-28 06:51:55 -0300

    feat: add PostMapper to map PostDTO to Post and Post to PostDTO

---
+++ Mappers/PostMapper.cs
@@ -0,0 +1,40 @@
+﻿using DevBlog.DTOs;
+using DevBlog.Entities;
+
+namespace DevBlog.Mappers
+{
+    public static class PostMapper
+    {
+
+        public static PostDTO ToDTO(Post post)
+        {
+            return new PostDTO
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Content = post.Content,
+                ImageUrl = post.ImageUrl,
+                CreatedAt = post.CreatedAt,
+                CategoryName = post.Category.Name,
+                AuthorName = post.Author.Name,
+                Tags = post.Tags.Select(TagMapper.ToDTO).ToList(),
+                Comments = post.Comments.Select(CommentMapper.ToDTO).ToList()
+            };
+
+        }
+
+        public static Post ToEntity(PostDTO postDTO)
+        {
+            return new Post
+            {
+                Id = postDTO.Id,
+                Title = postDTO.Title,
+                Content = postDTO.Content,
+                ImageUrl = postDTO.ImageUrl,
+                CreatedAt = postDTO.CreatedAt,
+                Tags = postDTO.Tags.Select(TagMapper.ToEntity).ToList(),
+                Comments = postDTO.Comments.Select(CommentMapper.ToEntity).ToList()
+            };
+        }
+    }
+}
