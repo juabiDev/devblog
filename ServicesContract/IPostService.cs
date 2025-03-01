cafcca2f8bb4d7ad3d1a70b3c885ce2d83c494c6
juabidev <Mederos40@outlook.com>
2025-02-28 21:57:19 -0300

    refactor: change return type List<Post> to List<PostDTO>

--- ServicesContract/IPostService.cs
+++ ServicesContract/IPostService.cs
@@ -7,11 +7,11 @@ namespace DevBlog.ServicesContract
     {
         Task CreatePost(CreatePostRequest post);
         Task<Post> UpdatePost(Post post);
-        Task<Post> DeletePost(Guid id);
+        Task DeletePost(Guid id);
         Task<Post> GetPostById(Guid id);
         Task<List<PostDTO>> GetAllPosts();
-        Task<List<Post>> GetPostsByCategory(Guid categoryId);
-        Task<List<Post>> GetPostsByTag(Guid tagId);
-        Task<List<Post>> GetPostsByAuthor(Guid authorId);
+        Task<List<PostDTO>> GetPostsByCategory(Guid categoryId);
+        Task<List<PostDTO>> GetPostsByTag(Guid tagId);
+        Task<List<PostDTO>> GetPostsByAuthor(Guid authorId);
     }
 }
