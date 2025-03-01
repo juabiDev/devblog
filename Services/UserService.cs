151fb8e03e6a09c55c29d4d85cb02cad98be15d0
juabidev <Mederos40@outlook.com>
2025-02-28 21:31:37 -0300

    feat: add validations and add userId into EditUserAsync method

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -41,6 +41,11 @@ namespace DevBlog.Services
                 throw new ArgumentNullException(nameof(user), "User cannot be null.");
             }
 
+            if (_db.User.Any(e => e.Email == user.Email))
+            {
+                throw new ArgumentException("User email already exists");
+            }
+
             // Execute the operation with the resilience pipeline
             try
             {
@@ -71,11 +76,26 @@ namespace DevBlog.Services
             {
                 await _resiliencePipeline.ExecuteAsync(async token =>
                 {
-                    var user = await _db.User.FindAsync(id);
+                    var user = await _db.User
+                        .Include(p => p.Followers)
+                        .Include(p => p.Posts)
+                        .AsSplitQuery()
+                        .IgnoreQueryFilters()
+                        .FirstOrDefaultAsync(e => e.Id == id, token);
+
+
                     if (user == null)
                     {
-                        throw new ArgumentNullException(nameof(user), "El usuario no existe.");
+                        throw new ArgumentException("User not found");
                     }
+
+                    if(user.DeletedAt != null)
+                    {
+                        throw new ArgumentException("User already deleted");
+                    }
+
+                    Console.WriteLine(user.DeletedAt);
+
                     user.DeletedAt = DateTime.UtcNow;
                     _db.User.Update(user);
                     await _db.SaveChangesAsync(token);
@@ -89,24 +109,31 @@ namespace DevBlog.Services
             {
                 throw new Exception("Error al conectar la base de datos");
             }
+            catch(ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
             catch (Exception ex)
             {
                 throw new Exception("Ocurrió un error inesperado al eliminar el usuario.");
             }
         }
 
-        public async Task EditUserAsync(UserDTO user)
+        public async Task EditUserAsync(Guid userId, UserDTO user)
         {
             try
             {
                 await _resiliencePipeline.ExecuteAsync(async token =>
                 {
                     User userEntity = Mappers.UserMapper.ToEntity(user);
-                    var userFounded = await _db.User.Where(e => e.Email == userEntity.Email).FirstOrDefaultAsync(token);
+                    var userFounded = await _db.User
+                        .Where(e => e.Email == userEntity.Email && e.Id == userId)
+                        .AsSplitQuery()
+                        .FirstOrDefaultAsync(token);
                     
                     if (userFounded == null)
                     {
-                        throw new ArgumentNullException(nameof(user), "El usuario no existe.");
+                        throw new ArgumentException("User not found");
                     }
 
                     userFounded.Name = user.Name;
@@ -114,6 +141,8 @@ namespace DevBlog.Services
                     userFounded.Email = user.Email;
                     userFounded.ProfilePhoto = user.ProfilePhoto;
                     userFounded.About = user.About;
+                    userFounded.UpdatedAt = DateTime.UtcNow;
+
                     _db.User.Update(userFounded);
                     await _db.SaveChangesAsync(token);
                 });
@@ -126,9 +155,12 @@ namespace DevBlog.Services
             {
                 throw new Exception("Error al conectar la base de datos");
             }
+            catch(ArgumentException ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
                 throw new Exception("Ocurrió un error inesperado al editar el usuario.");
             }
         }
