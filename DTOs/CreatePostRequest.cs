b9955d0d1083d233e74f5d8bfd8b01058fae8796
juabidev <Mederos40@outlook.com>
2025-03-01 10:13:41 -0300

    feat: add validations

--- DTOs/CreatePostRequest.cs
+++ DTOs/CreatePostRequest.cs
@@ -1,12 +1,21 @@
-﻿namespace DevBlog.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBlog.DTOs
 {
     public class CreatePostRequest
     {
+        [Required(ErrorMessage = "Author id is required")]
         public Guid AuthorId { get; set; }
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Content is required")]
         public string Content { get; set; }
+        [Required(ErrorMessage = "Image url is required")]
         public string ImageUrl { get; set; }
+        [Required(ErrorMessage = "Category id is required")]
         public Guid CategoryId { get; set; }
+        [Required(ErrorMessage = "Tags are required")]
+        [MinLength(3, ErrorMessage = "At least three tags are required")]
         public ICollection<Guid> Tags { get; set; } = new List<Guid>();
     }
 }
--- DTOs/PostDTO.cs
+++ DTOs/PostDTO.cs
@@ -1,14 +1,20 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace DevBlog.DTOs
 {
     public class PostDTO
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
         public string Content { get; set; }
+        [Required(ErrorMessage = "Image url is required")]
         public string ImageUrl { get; set; }
         public DateTime CreatedAt { get; set; }
+        [Required(ErrorMessage = "Author name is required")]
         public string AuthorName { get; set; }
+        [Required(ErrorMessage = "Category name is required")]
         public string CategoryName { get; set; }
         public ICollection<TagDTO> Tags { get; set; } = new List<TagDTO>();
         public ICollection<CommentDTO> Comments { get; set; } = new List<CommentDTO>();
