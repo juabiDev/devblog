364cc8676ae7f616951e866ac3581b4bf2753096
juabidev <Mederos40@outlook.com>
2025-03-07 19:27:20 -0300

    feat: add CommentController

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,105 @@
+﻿using DevBlog.DTOs;
+using DevBlog.ServicesContract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBlog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetComentById(Guid id)
+        {
+            try
+            {
+                var comment = await _commentService.GetCommentAsync(id);
+                return Ok(comment);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+
+        // all properties have to be filled in the request body
+        public async Task<IActionResult> AddComment(CreateCommentRequest comment)
+        {
+            try
+            {
+                await _commentService.AddCommentAsync(comment);
+                return Ok(
+                    new
+                    {
+                        message = "Comment added successfully"
+                    }   
+                );
+            }
+            catch(ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateComment(CreateCommentRequest comment)
+        {
+            try
+            {
+                await _commentService.UpdateCommentAsync(comment);
+                return Ok(
+                    new
+                    {
+                        message = "Comment updated successfully"
+                    }
+                );
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}/{userEmail}")]
+        public async Task<IActionResult> DeleteComment(Guid id, string userEmail)
+        {
+            try
+            {
+                await _commentService.DeleteCommentAsync(id, userEmail);
+                return Ok(
+                    new
+                    {
+                        message = "Comment deleted successfully"
+                    }
+                );
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
