4654aa74edd3dcee0797face83a3e24a280d67a9
juabidev <Mederos40@outlook.com>
2025-03-07 19:28:11 -0300

    feat: add CreateCommentRequest

---
+++ DTOs/CreateCommentRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBlog.DTOs
+{
+    public class CreateCommentRequest
+    {
+        [Required(ErrorMessage = "User email is required")]
+        public string UserEmail { get; set; }
+        public Guid PostId { get; set; }
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; }
+    }
+}
