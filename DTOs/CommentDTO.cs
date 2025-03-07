0f5e3d3aeac69c5efd2b550e91cef62760959d75
juabidev <Mederos40@outlook.com>
2025-03-07 19:27:58 -0300

    feat: add validations

--- DTOs/CommentDTO.cs
+++ DTOs/CommentDTO.cs
@@ -1,12 +1,20 @@
-﻿namespace DevBlog.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBlog.DTOs
 {
     public class CommentDTO
     {
         public Guid Id { get; set; }
-        public string Text { get; set; }
+        [Required(ErrorMessage = "Content is required")]
+        [MinLength(10)]
+        [MaxLength(500)]
+        public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
+        [Required(ErrorMessage = "Author name is required")]
         public string AuthorName { get; set; }
+        [Required(ErrorMessage = "Author email is required")]
         public string AuthorEmail { get; set; }
+        [Required(ErrorMessage = "Author lastname is required")]
         public string AuthorLastName { get; set; }
         public string AuthorProfilePhoto { get; set; }
     }
