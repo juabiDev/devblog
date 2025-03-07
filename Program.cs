bd6bad75d08380077c8904ea8913b2cabc508e95
juabidev <Mederos40@outlook.com>
2025-03-07 19:26:24 -0300

    feat: add CommentService as a Service

--- Program.cs
+++ Program.cs
@@ -31,6 +31,7 @@ builder.Services.AddDbContext<BlogDbContext>(options =>
 // Add Services
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
 
 var app = builder.Build();
 
