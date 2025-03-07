f8634d9e4cc849d62a3ff5c55683fa99c6171edf
juabidev <Mederos40@outlook.com>
2025-03-07 19:31:16 -0300

    feat: add ICommentService

---
+++ ServicesContract/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using DevBlog.DTOs;
+
+namespace DevBlog.ServicesContract
+{
+    public interface ICommentService
+    {
+        Task AddCommentAsync(CreateCommentRequest comment);
+        Task DeleteCommentAsync(Guid id, string userEmail);
+        Task UpdateCommentAsync(Guid id, CreateCommentRequest comment);
+        Task<IEnumerable<CommentDTO>> GetCommentsAsync(Guid postId);
+        Task<CommentDTO> GetCommentAsync(Guid id);
+        Task<CommentDTO> GetCommentAsync(Guid id, Guid userId);
+    }
+}
