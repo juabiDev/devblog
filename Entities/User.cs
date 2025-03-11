ddab04d8f1e444cf0211bfc666602916b911ce91
juabidev <Mederos40@outlook.com>
2025-03-11 10:38:10 -0300

    refactor: change property name

--- Entities/User.cs
+++ Entities/User.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string LastName { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string? ProfilePhoto { get; set; }
         public string? About { get; set; }
