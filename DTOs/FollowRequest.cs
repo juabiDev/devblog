f5f21cade503a252bb90e15bd7b831bff5947fc9
juabidev <Mederos40@outlook.com>
2025-03-11 10:36:19 -0300

    feat: add FollowRequest class to hide Follow Entity properties

---
+++ DTOs/FollowRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBlog.DTOs
+{
+    public class FollowRequest
+    {
+        [Required(ErrorMessage = "FollowerUserName is required")]
+        public string FollowerUserName { get; set; }
+        [Required(ErrorMessage = "FollowedUserName is required")]
+        public string FollowedUserName { get; set; }
+    }
+}
