5a965eaedb8edd956c1749b6fafb3f07f17cc006
juabidev <Mederos40@outlook.com>
2025-03-11 10:38:26 -0300

    feat: add ToDTO and ToEntity Methods

---
+++ Mappers/CategoryMapper.cs
@@ -0,0 +1,26 @@
+﻿using DevBlog.DTOs;
+using DevBlog.Entities;
+
+namespace DevBlog.Mappers
+{
+    public static class CategoryMapper
+    {
+        public static CategoryDTO ToDTO(Category category)
+        {
+            return new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public static Category ToEntity(CategoryDTO category)
+        {
+            return new Category
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+    }
+}
