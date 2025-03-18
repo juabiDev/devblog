8ed2bb1ebac1c2c18b3ec48a21ce7973ff5ac57c
juabidev <Mederos40@outlook.com>
2025-03-18 19:05:14 -0300

    feat: add CategoryController to provide routes to get Categories data

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using DevBlog.DTOs;
+using DevBlog.ServicesContract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevBlog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                return Ok(await _categoryService.GetCategoriesAsync());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategory(Guid id)
+        {
+            try
+            {
+                var category = await _categoryService.GetCategoryAsync(id);
+                return Ok(category);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+    }
+}
