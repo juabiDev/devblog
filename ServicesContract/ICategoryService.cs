22fa8a5d0215f9cb182aecfbb9aa43d2b45ecf93
juabidev <Mederos40@outlook.com>
2025-03-18 19:04:05 -0300

    feat: add ICategoryService

---
+++ ServicesContract/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using DevBlog.DTOs;
+
+namespace DevBlog.ServicesContract
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<CategoryDTO> GetCategoryAsync(Guid id);
+    }
+}
