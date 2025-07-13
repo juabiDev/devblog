95cfbceac46d9fe2970799cced48242c7728d77f
juabidev <Mederos40@outlook.com>
2025-07-13 20:52:36 -0300

    feat: add respositories contracts

---
+++ RepositoriesContracts/RepositoriesContracts/IFollowRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entities;
+using ServicesContracts.DTOs;
+
+namespace RepositoriesContracts.RepositoriesContracts;
+
+public interface IFollowRepository
+{
+	Task FollowUserAsync(FollowRequest followRequest);
+
+	Task UnfollowUserAsync(FollowRequest unfollowRequest);
+
+	Task<IEnumerable<Follow>> GetFollowersAsync(string username);
+
+	Task<IEnumerable<Follow>> GetFollowingAsync(string username);
+
+	Task<bool> IsFollowingAsync(string followerUsername, string followedUsername);
+}
