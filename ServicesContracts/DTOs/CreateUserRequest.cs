eae8a41da1e4fa9463b00d2b35e124441945d081
juabidev <Mederos40@outlook.com>
2025-07-18 23:51:01 -0300

    feat: create CreateUserRequest for hiding user entity information

---
+++ ServicesContracts/DTOs/CreateUserRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.DTOs
+{
+    public class CreateUserRequest
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(12, ErrorMessage = "Password must have at least 12 characters")]
+        public string Password { get; set; }
+        public string? ProfilePhoto { get; set; }
+
+        public string? About { get; set; }
+    }
+}
