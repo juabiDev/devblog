eaf7954120e2dbfcab1c192c4a5488df9de843fe
juabidev <Mederos40@outlook.com>
2025-07-18 23:49:49 -0300

    feat: adding JwtService for providing a Json Web Token when user is created or sign in the website

---
+++ Services/Services/JwtService.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ServicesContracts.DTOs;
+using ServicesContracts.ServicesContracts;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public AuthenticationResponse CreateJwtToken(LoginRequest user)
+        {
+           DateTime expiration = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["Jwt:EXPIRATION_HOURS"]));
+
+            Claim[] claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email.ToString()), // Subject (user id)
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT unique ID
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), // Issued at (date time and time of token generation)
+                new Claim(ClaimTypes.NameIdentifier, user.Email.ToString()), // Unique name identifier of the user (Email)
+
+            };
+
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])    
+            );
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenGenerator = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: expiration,
+                signingCredentials: signingCredentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            string token = handler.WriteToken(tokenGenerator);
+
+            return new AuthenticationResponse()
+            {
+                Token = token,
+                Expiration = expiration,
+                Email = user.Email
+            };
+        }
+
+        public AuthenticationResponse CreateJwtToken(UserDTO user)
+        {
+            DateTime expiration = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["Jwt:EXPIRATION_HOURS"]));
+
+            Claim[] claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email.ToString()), // Subject (user id)
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT unique ID
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), // Issued at (date time and time of token generation)
+                new Claim(ClaimTypes.NameIdentifier, user.Email.ToString()), // Unique name identifier of the user (Email)
+
+            };
+
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+            );
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenGenerator = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: expiration,
+                signingCredentials: signingCredentials
+            );
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            string token = handler.WriteToken(tokenGenerator);
+
+            return new AuthenticationResponse()
+            {
+                Token = token,
+                Expiration = expiration,
+                Email = user.Email
+            };
+        }
+
+
+    }
+}
---
+++ ServicesContracts/ServicesContracts/IJwtService.cs
@@ -0,0 +1,15 @@
+﻿using ServicesContracts.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.ServicesContracts
+{
+    public interface IJwtService
+    {
+        AuthenticationResponse CreateJwtToken(LoginRequest user);
+        AuthenticationResponse CreateJwtToken(UserDTO user);
+    }
+}
