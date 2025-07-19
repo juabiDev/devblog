17b39f8e0c829aa2ff6ffec12482407b96feef1a
juabidev <Mederos40@outlook.com>
2025-07-18 23:50:37 -0300

    feat: create AuthenticationResponse

---
+++ ServicesContracts/DTOs/AuthenticationResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesContracts.DTOs
+{
+    public class AuthenticationResponse
+    {
+        public string? UserName { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+        public string? Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+    }
+}
