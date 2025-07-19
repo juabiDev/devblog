fd7619525a0b3518b2c986881b92ab1778b3879f
juabidev <Mederos40@outlook.com>
2025-07-18 23:44:14 -0300

    feature: add AuthUserController for managing user login

---
+++ DevBlog/Controllers/AuthUserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicesContracts.DTOs;
+using ServicesContracts.ServicesContracts;
+
+namespace DevBlog.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthUserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IJwtService _jwtService;
+
+        public AuthUserController(IUserService userService, IJwtService jwtService) { 
+            _userService = userService;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("/login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest userLogin)
+        {
+            try
+            {
+                var user = await _userService.Login(userLogin);
+
+                if (user == null)
+                {
+                    return Unauthorized("Invalid credentials");
+                }
+
+                return Ok(_jwtService.CreateJwtToken(user));
+            }
+            catch(ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "El sistema no esta disponible en estos momentos");
+            }
+        }
+        
+    }
+}
